dc2b58707cc2cf45362516d5b700048c61467b86
vasusharrma <sharmavashu099@gmail.com>
2025-01-01 13:26:57 +0530

    util promises

--- AKBEND/first/index.js
+++ AKBEND/first/index.js
@@ -1,3 +1,34 @@
-const {readFile} = require('fs');
+const { readFile, writeFile } = require("fs").promises;
 
-readFile('./father/child/file-async.txt')
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFil
+
+
+const start = async () => {
+    try {
+        const first = await readFile("./father/first.txt" , 'utf8' );
+        const second = await readFile('./father/second.txt','utf8');
+        await writeFile('./father/util-fs-promises.txt',`Fir : ${first} and Sec: ${second}`,{flag:'a'});
+        console.log(first);
+        console.log(second);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
+// const getContent = (path) => {
+    //   return new Promise((resolve, reject) => {
+        //     readFile(path, "utf8", (err, data) => {
+            //       if (err) {
+                //         reject(err);
+//       } else {
+    //         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getContent('./father/file-async.txt').then((result) => console.log(result)).catch((error)=> console.log(error));

a4161bff20bd0b54a6a5a465a28af5cb6e708ab3
vasusharrma <sharmavashu099@gmail.com>
2025-02-01 11:43:17 +0530

    mongodb

--- AKBEND/second/database.js
+++ AKBEND/second/database.js
@@ -6,18 +6,51 @@ const URI
 // we will connect to database here
 
 const client = new MongoClient(URI);
-const dbName = 'vasuatlas';
+const dbName = 'pg';
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('pg');
+  const collection = db.collection('students');
 
   // the following code examples can be pasted here...
+  // read the document 
+  //
+  // const findResult = await collection.find({}).toArray();
+  // console.log("Result is => ", findResult);
+  //
+  // //insert the data
+  // const idata = {
+  //   name: "Ashutosh Sharma",
+  //   address: "Saharanpur"
+  // };
+  //
+  // const insertDocument = await collection.insertMany([idata]);
+  // const ResutlAi = await collection.find({}).toArray();
+  //
+  // console.log("After inserting document the data is : ", ResutlAi);
+  //
+  // 
+  // const docToIn = {
+  //   name: "Nikhil Rana",
+  //   address: "Nanauta"
+  // }
+  //
+  // const dataInsert = await collection.insertOne(docToIn);
+  //
+  // const readDocument = await collection.find({}).toArray();
+  // console.log("document is : ", readDocument);
+  //
+  //
+  //
+
+  const readAshu = await collection.find({ name: "Ashutosh Sharma" }).toArray();
+  console.log("ashu is : ", readAshu);
 
   return 'done.';
+
 }
 
 main()
